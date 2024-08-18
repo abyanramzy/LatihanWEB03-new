aab79a077322add8a61c6f2949671d627c024363
abyanramzy <abyanr.a23@gmail.com>
2024-08-18 23:13:05 +0700

    progress3

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    var image = document.getElementById('animatedImage');
+    image.classList.toggle('hover'); // Menambah/menghapus kelas hover
+});
